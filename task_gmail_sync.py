e39ad2fe9926dfb944dfd2c8185c68815ebfeb3a
Brian Hulette <hulettbh@gmail.com>
2019-04-29 08:21:54 -0700

    Use etc for credentials file, use XDG_CACHE_DIR for token.pickle

--- task_gmail_sync.py
+++ task_gmail_sync.py
@@ -6,28 +6,35 @@ from taskw import TaskWarrior
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from xdg import BaseDirectory
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 LABEL = "gtd/next-action"
 
+PKGNAME = 'task-gmail-sync'
+CACHE_DIR = BaseDirectory.save_cache_path(PKGNAME)
+TOKEN_PATH = os.path.join(CACHE_DIR, "token.pickle")
+CREDENTIALS_PATH = os.path.join("/etc", PKGNAME, "credentials.json")
 
 def initGmailService():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, "rb") as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            if not os.path.exists(CREDENTIALS_PATH):
+                raise ValueError("No credentials file found! Please install one at '{0}'".format(CREDENTIALS_PATH))
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open("token.pickle", "wb") as token:
+        with open(TOKEN_PATH, "wb") as token:
             pickle.dump(creds, token)
 
     service = build("gmail", "v1", credentials=creds)
